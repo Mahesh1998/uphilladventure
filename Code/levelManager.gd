5f4f530aef9d3eea685bf4f01feb27f836b119f3
VivekPyda <vivek.pyda@gmail.com>
2024-10-23 09:50:58 -0600

    Fixes for coins and fuel mechanisms

--- Code/levelManager.gd
+++ Code/levelManager.gd
@@ -6,7 +6,7 @@ func add_coins(amount):
 	coins_collected += amount
 	$UI/Coin/Label.text = str(coins_collected)
 
-#func update_fuel_UI(value):
-	#$UI/Fuel/ProgressBar.value = value
-	#var stylebox = $UI/Fuel/ProgressBar.get("custom_styles/fg")
-	#stylebox.bg_color.h = lerp(0, 0.3, value / 100)       
+func update_fuel_UI(value):
+	$UI/Fuel/ProgressBar.value = value
+	var stylebox = $UI/Fuel/ProgressBar.get("theme_override_styles/background")
+	stylebox.bg_color = Color("#333333")      
--- Code/player.gd
+++ Code/player.gd
@@ -3,32 +3,41 @@ extends RigidBody2D
 var wheels = []
 var speed = 60000
 var max_speed = 50
-
 var fuel = 100
+
 func _ready():
 	wheels = get_tree().get_nodes_in_group("wheel")
 	#get_parent().update_fuel_UI(fuel)
-	
-	
+
+
 func _physics_process(delta):
 	if fuel > 0:
-		if Input.is_action_just_pressed("ui_right"):
+		if Input.is_action_pressed("ui_right"):
 			use_fuel(delta)
 			for wheel in wheels:
 				if wheel.angular_velocity < max_speed:
-					wheel.apply_torque_impulse(speed * delta * 250)
+					wheel.apply_torque_impulse(speed * delta * 60)
 				
-		if Input.is_action_just_pressed("ui_left"):
+		if Input.is_action_pressed("ui_left"):
 			use_fuel(delta)
 			for wheel in wheels:
 				if wheel.angular_velocity >  -max_speed:
-					wheel.apply_torque_impulse(-speed * delta * 100)	
-	print(fuel)
+					wheel.apply_torque_impulse(-speed * delta * 60)
+	else:
+		if $GameOverTimer.is_stopped():
+			$GameOverTimer.start()
+
+
 func refuel():
 	fuel = 100
-	#get_parent().update_fuel_UI(fuel)
-	
+	get_parent().update_fuel_UI(fuel)
+
+
 func use_fuel(delta):
-	fuel -= 10* delta
-	fuel = clamp(fuel,0,100)
-	#get_parent().update_fuel_UI(fuel)
+	fuel -= 10 * delta
+	fuel = clamp(fuel, 0, 100)
+	get_parent().update_fuel_UI(fuel)
+
+
+func _on_game_over_timer_timeout() -> void:
+	get_tree().reload_current_scene()
