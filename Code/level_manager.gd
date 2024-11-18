8b1201aa074054958e7ea70b102c6d1723bc8964
VivekPyda <vivek.pyda@gmail.com>
2024-11-18 10:12:20 -0700

    Fixed error with updating fuel bar and coins collected

--- Code/level_manager.gd
+++ Code/level_manager.gd
@@ -4,10 +4,7 @@ var coins_collected = 0
 
 func add_coins(amount):
 	coins_collected += amount
-	#$UI/Coin/Label.text = str(coins_collected)
+	$CanvasLayer/LevelUI/VBoxContainer/CurrencyDisplayCoins/CoinsCollected.text = str(coins_collected)
 
 func update_fuel_UI(value):
-	#$CanvasLayer/LevelUI.value = value
-	#var stylebox = $UI/Fuel/ProgressBar.get("theme_override_styles/background")
-	#stylebox.bg_color = Color("#333333")
-	pass
+	$CanvasLayer/LevelUI/VBoxContainer/HBoxContainer/FuelBar.value = value
